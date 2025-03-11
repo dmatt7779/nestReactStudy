cb6b87c52a3af3b893b2f77371ed9915aaec7c2c
Mateo Aguilar <mateoaguilar@MacBook-Pro-de-Lucia.local>
2025-03-11 09:54:54 -0500

    merge and conflicts resolved

--- magicFront/mi-app-login/src/newProject/newProject.js
+++ magicFront/mi-app-login/src/newProject/newProject.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import '../styles.css';
+import '../style/styles.css';
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Icon1 from '../images/edit-icon.png';
--- magicFront/mi-app-login/src/projectInfo/projectInfo.js
+++ magicFront/mi-app-login/src/projectInfo/projectInfo.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import '../styles.css';
+import '../style/styles.css';
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import profe1 from '../images/profe1.jpg';
